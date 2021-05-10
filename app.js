9f58b7b57f6fa58356a23926af6016770cde4899
Tuan <longtuan03@gmail.com>
2021-05-11 00:42:38 +0700

    met

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/user.api");
-
+const cors = require("cors");
 const app = express();
 require("dotenv").config();
 app.use(logger("dev"));
@@ -13,9 +13,34 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-const cors = require("cors");
+
 app.use("/api", indexRouter);
 app.use(cors());
+
+// app.use(function (req, res, next) {
+//   // Website you wish to allow to connect
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:8888");
+
+//   // Request methods you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+//   );
+
+//   // Request headers you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "X-Requested-With,content-type"
+//   );
+
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+
+//   // Pass to next layer of middleware
+//   next();
+// });
+
 mongoose
   .connect(process.env.DB_API, {
     useCreateIndex: true,
